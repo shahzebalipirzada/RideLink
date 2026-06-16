6c5edcde62e19955e369faa3adc1ffc6a79f6f65
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2026-06-16 15:40:11 +0500

    initial commit

---
+++ src/main/java/com/mrshaikhmuhammad/ridelink/RideLinkApplication.java
@@ -0,0 +1,13 @@
+package com.mrshaikhmuhammad.ridelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrshaikhmuhammad/ridelink/RideLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrshaikhmuhammad.ridelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
