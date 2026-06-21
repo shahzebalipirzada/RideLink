15ae159cccf2b5c102180cc7754af47038f61525
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2026-06-22 01:16:00 +0500

    removed unnecessary complex fields from Path

--- src/main/java/com/mrshaikhmuhammad/ridelink/entity/Path.java
+++ src/main/java/com/mrshaikhmuhammad/ridelink/entity/Path.java
@@ -1,12 +1,9 @@
 package com.mrshaikhmuhammad.ridelink.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
 public record Path(
         String code,
-        List<Route> routes
+        Route routes
 ) {
     public record Route(
             double distance,
@@ -15,11 +12,6 @@ public record Path(
 
             @JsonProperty("weight_name")
             String weightName,
-            Geometry geometry
-    ) {}
-
-    public record Geometry(
-            String type,                      // "LineString"
-            List<List<Double>> coordinates    // [[lng, lat], [lng, lat], ...]
+            String geometry
     ) {}
 }
