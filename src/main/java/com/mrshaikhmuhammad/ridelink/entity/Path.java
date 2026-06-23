e9423c2d3d4a90146959cf1dc071662ce7bccce9
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2026-06-23 18:59:47 +0500

    included waypoints

--- src/main/java/com/mrshaikhmuhammad/ridelink/entity/Path.java
+++ src/main/java/com/mrshaikhmuhammad/ridelink/entity/Path.java
@@ -1,17 +1,38 @@
 package com.mrshaikhmuhammad.ridelink.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
+import java.util.*;
+
 public record Path(
         String code,
-        Route route
+        List<Route> route,
+        List<Waypoint> waypoints
 ) {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record Route(
             double distance,
             double duration,
             double weight,
+            List<Leg> legs,
 
             @JsonProperty("weight_name")
             String weightName,
             String geometry
+    ) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Waypoint(
+            String hint,
+            List<Double> location,  // [longitude, latitude]
+            String name,
+            double distance
+    ) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Leg(
+            double duration,
+            double distance,
+            double weight
     ) {}
 }
