f229ec5836f295bd2e549dca3c39d2d70fb1d0bd
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2026-06-24 13:06:45 +0500

    implemented geospatial indexing using mongodb

--- src/main/java/com/mrshaikhmuhammad/ridelink/entity/GeoPoint.java
+++ src/main/java/com/mrshaikhmuhammad/ridelink/entity/GeoPoint.java
@@ -1,10 +1,19 @@
 package com.mrshaikhmuhammad.ridelink.entity;
 
+import lombok.Data;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+
 import java.util.Locale;
 
-public record GeoPoint(double longitude, double latitude) {
+@Data
+public class GeoPoint extends GeoJsonPoint {
+
+    public GeoPoint(double longitude, double latitude){
+        super(longitude, latitude);
+    }
+
     @Override
     public String toString(){
-        return String.format("%.6f,%.6f", longitude, latitude);
+        return String.format("%.6f,%.6f", getX(), getY());
     }
 }
