96fecdcf68cf467026020ae93f030e9f062a31bc
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2026-06-29 22:39:40 +0500

    improved readability of endpoints

--- src/main/java/com/mrshaikhmuhammad/ridelink/controller/RideController.java
+++ src/main/java/com/mrshaikhmuhammad/ridelink/controller/RideController.java
@@ -1,6 +1,7 @@
 package com.mrshaikhmuhammad.ridelink.controller;
 
-import com.mrshaikhmuhammad.ridelink.entity.Ride;
+import com.mrshaikhmuhammad.ridelink.dto.request.Ride;
+import com.mrshaikhmuhammad.ridelink.dto.response.RideMatch;
 import com.mrshaikhmuhammad.ridelink.service.RideService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("ride")
+@RequestMapping("/ride")
 public class RideController {
 
     @Autowired
     RideService rideService;
 
-    @PostMapping
+    @PostMapping("save")
     public ResponseEntity<?> saveRides(@RequestBody Ride ride){
         try{
             rideService.saveRide(ride);
@@ -26,11 +27,11 @@ public class RideController {
         }
     }
 
-    @GetMapping("/{radius}")
+    @PostMapping("/search/{radius}")
     public ResponseEntity<?> searchRides(@RequestBody Ride ride, @PathVariable int radius){
         try{
-            List<Ride> rides = rideService.searchRides(ride, radius);
-            return new ResponseEntity<>(rides, HttpStatus.CREATED);
+            List<RideMatch> rides = rideService.searchRides(ride, radius);
+            return new ResponseEntity<>(rides, HttpStatus.OK);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
