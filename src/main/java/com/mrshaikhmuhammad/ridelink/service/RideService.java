a282fbd43d74243d0a276e6ad2eda92b6fe590a9
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2026-06-29 22:48:24 +0500

    reduced complexity of response for frontend

--- src/main/java/com/mrshaikhmuhammad/ridelink/service/RideService.java
+++ src/main/java/com/mrshaikhmuhammad/ridelink/service/RideService.java
@@ -1,6 +1,7 @@
 package com.mrshaikhmuhammad.ridelink.service;
 
-import com.mrshaikhmuhammad.ridelink.entity.*;
+import com.mrshaikhmuhammad.ridelink.dto.request.Ride;
+import com.mrshaikhmuhammad.ridelink.dto.response.RideMatch;
 import com.mrshaikhmuhammad.ridelink.external.osrm.OsrmRouteClient;
 import com.mrshaikhmuhammad.ridelink.repository.RideRepository;
 import com.mrshaikhmuhammad.ridelink.service.scoring.RideScore;
@@ -25,28 +26,29 @@ public class RideService {
     @Autowired
     RideScore similarityScorer;
 
-    private final int MAX_WAIT_SECONDS = 120*60;
-
-    public List<Ride> searchRides(Ride requestRide, int radius){
-        List<Ride> candidates = filterRide(requestRide, radius);
-
-        return candidates.stream()
-                .map(candidate -> Map.entry(candidate, similarityScorer.score(requestRide, candidate)))
-                .sorted(Map.Entry.<Ride, Double>comparingByValue().reversed())
-                .map(Map.Entry::getKey)
-                .toList();
-    }
+    private final int MAX_WAIT_SECONDS = 2*60*60;
 
     public void saveRide(Ride ride){
         ride.setPath(osrmClient);
         rideRepository.save(ride);
     }
 
+    public List<RideMatch> searchRides(Ride requestRide, int radius){
+        requestRide.setPath(osrmClient);
+        List<Ride> candidates = filterRide(requestRide, radius);
+
+        return candidates.stream()
+                .map(candidate -> Map.entry(candidate, similarityScorer.score(requestRide, candidate)))
+                .sorted(Map.Entry.<Ride, Double>comparingByValue().reversed())
+                .map(entry -> new RideMatch(entry.getKey()))
+                .toList();
+    }
+
     private List<Ride> filterRide(Ride requestRide, int radius){
         Criteria criteria = new Criteria().andOperator(
                 Criteria.where("_id").ne(requestRide.getId()),
 
-                Criteria.where("departureTime")
+                Criteria.where("departure-time")
                         .gt(requestRide.getDepartureTime().minusSeconds(MAX_WAIT_SECONDS))
                         .lt(requestRide.getDepartureTime().plusSeconds(MAX_WAIT_SECONDS)),
 
