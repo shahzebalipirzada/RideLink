5fe0e52c4b566012a417e2bb9142c2f5e8963487
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2026-06-29 22:43:45 +0500

    handled edge cases of serialization

--- src/main/java/com/mrshaikhmuhammad/ridelink/entity/Role.java
+++ src/main/java/com/mrshaikhmuhammad/ridelink/entity/Role.java
@@ -1,6 +1,13 @@
 package com.mrshaikhmuhammad.ridelink.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Role {
     DRIVER,
-    PASSENGER
+    PASSENGER;
+
+    @JsonCreator
+    public static Role fromValue(String value) {
+        return Role.valueOf(value.toUpperCase());
+    }
 }
